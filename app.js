a1c5706878914871a29c3d3fc945f41752ec1e72
Charles Loehle <clguitar1@gmail.com>
2018-11-01 09:25:12 -0400

    created the comment model

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000, // npm run server
   bodyParser = require("body-parser"),
-  mongoose = require("mongoose");
-Campground = require("./models/campground");
-seedDB = require("./seeds");
-
-seedDB();
+  mongoose = require("mongoose"),
+  Campground = require("./models/campground"),
+  seedDB = require("./seeds");
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+seedDB();
 
 app.get('/', (req, res) => res.render('landing'));
 
@@ -51,7 +50,7 @@ app.get("/campgrounds/new", (req, res) => {
 // Show - Show a specific campground
 app.get("/campgrounds/:id", (req, res) => {
   //find the campground with provided id
-  Campground.findById(req.params.id).populate.exec((err, foundCampground) => {
+  Campground.findById(req.params.id).populate("comments").exec((err, foundCampground) => {
     if (err) {
       console.log(err);
     } else {
