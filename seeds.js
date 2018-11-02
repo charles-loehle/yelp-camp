b938006f9f68773ac90ed62b882e30da732457f7
Charles Loehle <clguitar1@gmail.com>
2018-11-02 09:43:21 -0400

    linked main.css file

--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data = [
   {
     name: "Sunnyvale Camp",
     image: "https://images.unsplash.com/photo-1525811902-f2342640856e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1a7383ad093ffea99d373681b9974056&auto=format&fit=crop&w=800&q=60",
-    description: "blah blah blah"
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium corporis molestiae minus, minima, quo ipsum fugiat voluptatibus, voluptatem adipisci saepe inventore neque laboriosam id. Inventore delectus dicta harum reiciendis quas?"
   },
   {
     name: "Cloud's Rest",
     image: "https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=343c64df1b43f50769656d03c2b9f523&auto=format&fit=crop&w=800&q=60",
-    description: "blah blah blah"
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium corporis molestiae minus, minima, quo ipsum fugiat voluptatibus, voluptatem adipisci saepe inventore neque laboriosam id. Inventore delectus dicta harum reiciendis quas?"
   },
   {
     name: "Desert Mesa",
     image: "https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d95171e276fbd03de651f9aecb64b53d&auto=format&fit=crop&w=800&q=60",
-    description: "blah blah blah"
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium corporis molestiae minus, minima, quo ipsum fugiat voluptatibus, voluptatem adipisci saepe inventore neque laboriosam id. Inventore delectus dicta harum reiciendis quas?"
   }
 ]
 
