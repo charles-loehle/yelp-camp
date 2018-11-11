6a9477f5b6a99399d57d7213846ac84415573207
Charles Loehle <clguitar1@gmail.com>
2018-11-11 08:07:56 -0500

    finished destroy and authorization

--- routes/comments.js
+++ routes/comments.js
@@ -45,7 +45,7 @@ router.post("/", isLoggedIn, (req, res) => {
 });
 
 // COMMENT EDIT
-router.get("/:comment_id/edit", (req, res) => {
+router.get("/:comment_id/edit", checkCommentOwnership, (req, res) => {
   Comment.findById(req.params.comment_id, (err, foundComment) => {
     if (err) {
       res.redirect("back")
@@ -56,8 +56,19 @@ router.get("/:comment_id/edit", (req, res) => {
 })
 
 // COMMENT UPDATE
-router.put("/:comment_id", (req, res) => {
+router.put("/:comment_id", checkCommentOwnership, (req, res) => {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+    if (err) {
+      res.redirect("back")
+    } else {
+      res.redirect("/campgrounds/" + req.params.id)
+    }
+  })
+})
+
+// COMMENT DESTROY ROUTE 
+router.delete("/:comment_id", checkCommentOwnership, (req, res) => {
+  Comment.findByIdAndRemove(req.params.comment_id, (err) => {
     if (err) {
       res.redirect("back")
     } else {
@@ -75,4 +86,26 @@ function isLoggedIn(req, res, next) {
   }
 }
 
+function checkCommentOwnership(req, res, next) {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+      if (err) {
+        res.redirect("back")
+      } else {
+        // does user own the comment?
+        if (foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back")
+        }
+      }
+    })
+  } else {
+    res.redirect("back")
+  }
+}
+
 module.exports = router;
+
+// Campground Destroy route: /campgrounds/:id
+// Comment Destroy route:    /campgrounds/:id/comments/:comment_id
